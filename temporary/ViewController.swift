ab69c9b4a02fdeea6301ece53e641ce321097029
Kim - leo <ochell2@naver.com>
2021-11-27 11:40:21 +0900

    hi

---
+++ temporary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  temporary
+//
+//  Created by 수현 on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var label = "world22"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
